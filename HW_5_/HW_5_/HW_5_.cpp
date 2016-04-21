d7578b96c406487f1ac30a936383e061321c2459
jkuczynski1 <jkuczynski1@luc.edu>
2016-04-21 12:01:31 -0500

    added rosettacode variants for pre-implementation.
    I know I will not fully complete this assignment today, and as a result plan on focusing on our upcoming Homework.
    
    I will do what I can, but still-the timing for these assignments was not ideal, in any way, shape, or form.

--- HW_5_/HW_5_/HW_5_.cpp
+++ HW_5_/HW_5_/HW_5_.cpp
@@ -25,7 +25,7 @@ const int MaxNum = 10001;//for a file of any size < max integer is 10,000.
 int timecounter = 0; //counter to check the number of steps for each sort method.
 time_t timer;
 time_t resultTimer;
-//*Merge sort code*\\
+//*Merge sort code*\\-lines 28-72
 //
 //lines 28-40 and 50, 52, 54 (excet timecounter++;) came from rosettacode.
 template<typename RandomAccessIterator, typename Order>
@@ -71,16 +71,154 @@ void Merge_sort(RandomAccessIterator first, RandomAccessIterator last)
 }
 //*end of code*\\
 
-//*Bubble sort code*\\
+//Bubble sort code\\-74 to 124
+
+//template <typename RandomAccessIterator>
+/*int bubble_sort(RandomAccessIterator begin, RandomAccessIterator end) {
+bool swapped = true;
+while (begin != end-- && swapped) {
+swapped = false;
+for (auto i = begin; i != end; ++i) {
+if (*(i + 1) < *i) {
+std::iter_swap(i, i + 1);
+swapped = true;
+}
+}
+}
+}
+
+int bubble_main() {
+int a[] = { 100, 2, 56, 200, -52, 3, 99, 33, 177, -199 };
+bubble_sort(std::begin(a), std::end(a));
+copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
+std::cout << "\n";
+}*\
 //*end of code*\\
+
+
 
 //*Insertion sort code*\\
+/*template <typename RandomAccessIterator, typename Predicate>
+/*
+void insertion_sort(RandomAccessIterator begin, RandomAccessIterator end,
+                    Predicate p) {
+  for (auto i = begin; i != end; ++i) {
+    std::rotate(std::upper_bound(begin, i, *i, p), i, i + 1);
+  }
+}
+ 
+template <typename RandomAccessIterator>
+void insertion_sort(RandomAccessIterator begin, RandomAccessIterator end) {
+  insertion_sort(
+      begin, end,
+      std::less<
+          typename std::iterator_traits<RandomAccessIterator>::value_type>());
+}
+ 
+int main() {
+  int a[] = { 100, 2, 56, 200, -52, 3, 99, 33, 177, -199 };
+  insertion_sort(std::begin(a), std::end(a));
+  copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
+  std::cout << "\n";
+}*/
 //*end of code*\\
 
-//*Quick sort code*\\
+//*Quick sort code*\\-126-145
+//template<typename RandomAccessIterator,
+//	typename Order>
+/*	void quicksort(RandomAccessIterator first, RandomAccessIterator last, Order order)
+{
+	if (last - first > 1)
+	{
+		RandomAccessIterator split = std::partition(first + 1, last, std::bind2nd(order, *first));
+		std::iter_swap(first, split - 1);
+		quicksort(first, split - 1, order);
+		quicksort(split, last, order);
+	}
+}
+
+template<typename RandomAccessIterator>
+void quicksort(RandomAccessIterator first, RandomAccessIterator last)
+{
+	quicksort(first, last, std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>());
+}*/
 //*end of code*\\
 
-//*Shell sort code*\\
+//*Shell sort code*\\-147-222
+/*class shell
+{
+public:
+	shell()
+	{
+		_gap[0] = 1750; _gap[1] = 701; _gap[2] = 301; _gap[3] = 132; _gap[4] = 57; _gap[5] = 23; _gap[6] = 10; _gap[7] = 4; _gap[8] = 1;
+	}
+
+	void sort(int* a, int count)
+	{
+		_cnt = count;
+		for (int x = 0; x < 9; x++)
+			if (count > _gap[x])
+			{
+				_idx = x; break;
+			}
+
+		sortIt(a);
+	}
+
+private:
+	void sortIt(int* arr)
+	{
+		bool sorted = false;
+		while (true)
+		{
+			sorted = true;
+			int st = 0;
+			for (int x = _gap[_idx]; x < _cnt; x += _gap[_idx])
+			{
+				if (arr[st] > arr[x])
+				{
+					swap(arr[st], arr[x]); sorted = false;
+				}
+				st = x;
+			}
+			if (++_idx >= 8) _idx = 8;
+			if (sorted && _idx == 8) break;
+		}
+	}
+
+	void swap(int& a, int& b) { int t = a; a = b; b = t; }
+
+	int _gap[9], _idx, _cnt;
+};
+//--------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	srand(static_cast<unsigned int>(time(NULL))); int arr[MAX];
+	for (int x = 0; x < MAX; x++)
+		arr[x] = rand() % MAX - rand() % MAX;
+
+	cout << " Before: \n=========\n";
+	for (int x = 0; x < 7; x++)
+	{
+		for (int a = 0; a < 18; a++)
+		{
+			cout << arr[x * 18 + a] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl; shell s; s.sort(arr, MAX);
+
+	cout << " After: \n========\n";
+	for (int x = 0; x < 7; x++)
+	{
+		for (int a = 0; a < 18; a++)
+		{
+			cout << arr[x * 18 + a] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl; return system("pause");
+}*/
 //*end of code*\\
 
 

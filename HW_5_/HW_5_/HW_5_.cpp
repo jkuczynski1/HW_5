988a7b19abf4e6012d53888ec7d2cd20d0771c7a
jkuczynski1 <jkuczynski1@luc.edu>
2016-04-20 22:47:17 -0500

    first push...base file, sources, etc;

---
+++ HW_5_/HW_5_/HW_5_.cpp
@@ -0,0 +1,22 @@
+/* HW_5_.cpp : Defines the entry point for the console application.
+
+my sources are Rosettacode and the textbook. I am not directly applying rosettacode code as my own, 
+but used it to double-check my thought process and influence my own. 
+
+I will mention what each  piece of code does, and if it was influenced by rosettacode.
+rosettacode links:
+https://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort#C.2B.2B
+https://rosettacode.org/wiki/Sorting_algorithms/Insertion_sort#C.2B.2B
+https://rosettacode.org/wiki/Sorting_algorithms/Quicksort#C.2B.2B
+https://rosettacode.org/wiki/Sorting_algorithms/Shell_sort#C.2B.2B
+*/
+//firt thing-includes...these are the same as rosettacode as I fould including these made things much easier overall.
+
+#include "stdafx.h"
+
+using namespace std;
+int main()
+{
+    return 0;
+}
+
